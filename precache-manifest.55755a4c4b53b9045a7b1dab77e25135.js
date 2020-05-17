0e4e9d9eddcc800095b723379c9eec54a44bcffc
Camilla <camilla.moeller@gmail.com>
2020-05-17 13:30:05 +0200

    Updates

---
+++ precache-manifest.55755a4c4b53b9045a7b1dab77e25135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044fb0904ba72e74f87428e7f682b119",
+    "url": "/Wishlist/index.html"
+  },
+  {
+    "revision": "8a578385844475054e17",
+    "url": "/Wishlist/static/css/main.86119eae.chunk.css"
+  },
+  {
+    "revision": "204c476c2220e843a149",
+    "url": "/Wishlist/static/js/2.a214d64f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wishlist/static/js/2.a214d64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a578385844475054e17",
+    "url": "/Wishlist/static/js/main.fc9c21de.chunk.js"
+  },
+  {
+    "revision": "e52852db84e3fd280ce7",
+    "url": "/Wishlist/static/js/runtime-main.91fb81c7.js"
+  }
+]);
