021ee748ec682e9927750cec531e557121aa2c9f
Camilla <camilla.moeller@gmail.com>
2020-05-24 20:50:07 +0200

    populate list from text file in dropbox instead of using the hardcoded list

--- src/WishContext.js
+++ src/WishContext.js
@@ -4,34 +4,34 @@ import {Dropbox} from 'dropbox';
 export const WishContext = createContext();
 
 export const WishProvider = props => {
-    const [wishes, setWishes] = useState([
-        {
-            description: 'Væglamper',
-            price: '300-400'
-        },
-        {
-            description: 'Roccamore sko',
-            price: '1999'
-        }
-    ])
+    const [wishes, setWishes] = useState({});
 
-    console.log(getWishlistFromApi());
+    const getWishlistFromApi = () => {
+        const accessToken = '8IfTC1NEM54AAAAAAAAifyUm0MYHloCbaYtpR6_xiVaYt3CJa3yR630_swRMnbQg';
+        const dbx = new Dropbox({  
+            accessToken,  
+            fetch  
+          });
+    
+        dbx.filesDownload({
+            path: '/wish.txt'
+        }).then(responseTxt => {
+            return responseTxt.fileBlob.text();
+        }).then(response => {
+            console.log(response);
+            var obj = JSON.parse(response);
+            console.log(obj);
+            setWishes(obj);
+        })
+        .catch(error => console.error(error));
+    }
+
+    //todo: WHY IS THIS REPEATED EVERY SECOND??
+    //getWishlistFromApi();
 
     return(
         <WishContext.Provider value={[wishes, setWishes]}>
             {props.children};
         </WishContext.Provider>
     );
-}
-
-async function getWishlistFromApi() {
-    const accessToken = '8IfTC1NEM54AAAAAAAAifyUm0MYHloCbaYtpR6_xiVaYt3CJa3yR630_swRMnbQg';
-    const dbx = new Dropbox({  
-        accessToken,  
-        fetch  
-      });
-
-    dbx.filesListFolder({  
-        path: ''  
-    }).then(response => console.log(response)); 
 }
