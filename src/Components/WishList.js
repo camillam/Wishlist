c636b7f3118f8e5f6c65e87ff36c4ae0c303ac48
Camilla <camilla.moeller@gmail.com>
2020-06-14 20:10:55 +0200

    adding loading to wishlist

--- src/Components/WishList.js
+++ src/Components/WishList.js
@@ -5,25 +5,29 @@
  const WishList = () => {
     const [wishes] = useContext(WishContext);
 
-    return(
-        <div>         
-            <div className="wrap-table100">
-            <div className="table100">
-            <table>
-                <tbody>
-                    <tr>
-                        <td className="column1"><b>Antal ønsker: {wishes.length}</b></td>
-                        <td className="column2"></td>
-                    </tr>
-                    {wishes.map(wish => (
-                        <Wish description={wish.description} price={wish.price} />
-                    ))} 
-                </tbody>
-            </table> 
-            </div>
-            </div>        
-        </div>       
-    );
+    if (wishes === undefined) { 
+        return(<div>Loading....</div>);
+    } else {
+        return(
+            <div>         
+                <div className="wrap-table100">
+                <div className="table100">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td className="column1"><b>Antal ønsker: {wishes.length}</b></td>
+                            <td className="column2"></td>
+                        </tr>
+                        {wishes.map(wish => (
+                            <Wish description={wish.description} price={wish.price} />
+                        ))} 
+                    </tbody>
+                </table> 
+                </div>
+                </div>        
+            </div>       
+        );
+    }
  }
 
  export default WishList;
